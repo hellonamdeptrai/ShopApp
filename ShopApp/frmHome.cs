643bf90dac2a236164173d73b8f80d93e297e3d9
Tuấn Nam <tuannam2505@gmail.com>
2021-09-30 01:11:17 +0700

    update 1

--- ShopApp/frmHome.cs
+++ ShopApp/frmHome.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ShopApp
 {
-    public partial class Form1 : Form
+    public partial class frmHome : Form
     {
-        public Form1()
+        public frmHome()
         {
             InitializeComponent();
         }
